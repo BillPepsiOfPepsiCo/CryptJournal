ec15cbc13e45b4297dd3858a2ba9bc04da526c2e
DoktuhParadox <doktuhparadox@gmail.com>
2014-06-28 08:27:41 -0500

    Documented NodeState and decreased the transition duration

--- src/com/doktuhparadox/cryptjournal/NodeState.java
+++ src/com/doktuhparadox/cryptjournal/NodeState.java
@@ -12,11 +12,20 @@ import javafx.util.Duration;
  * Date of creation: 6/27/14, at 1:17 PM.
  */
 public class NodeState {
+
+    /**
+     * Fades a node out and enables it.
+     * @param node
+     */
     public static void enable(Node node) {
-        Transitions.fade(node, Duration.millis(700), 0.5, 1.0, false);
+        Transitions.fade(node, Duration.millis(300), 0.5, 1.0, false);
     }
 
+    /**
+     * Fades a node in and disables it.
+     * @param node
+     */
     public static void disable(Node node) {
-        Transitions.fade(node, Duration.millis(700), 1.0, 0.5, true);
+        Transitions.fade(node, Duration.millis(300), 1.0, 0.5, true);
     }
 }
