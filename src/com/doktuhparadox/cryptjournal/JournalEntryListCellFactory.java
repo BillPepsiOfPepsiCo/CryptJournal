440b195ee8c7d56c04c6ba01d182f2d965e3ec84
DoktuhParadox <doktuhparadox@gmail.com>
2014-06-28 11:45:42 -0500

    Added creation date and time to list cells

--- src/com/doktuhparadox/cryptjournal/JournalEntryListCellFactory.java
+++ src/com/doktuhparadox/cryptjournal/JournalEntryListCellFactory.java
@@ -2,6 +2,9 @@ package com.doktuhparadox.cryptjournal;
 
 import javafx.scene.control.ListCell;
 
+import java.nio.file.Files;
+import java.io.IOException;
+
 /**
  * Created and written with IntelliJ IDEA.
  * Module of: CryptJournal
@@ -12,8 +15,21 @@ public class JournalEntryListCellFactory extends ListCell<JournalEntry> {
     @Override
     public void updateItem(JournalEntry item, boolean empty) {
         super.updateItem(item, empty);
+
         if (item != null) {
-            this.setText(item.getName());
+            try {
+                String zuluDate = Files.getAttribute(item.getFile().toPath(), "creationTime").toString();
+                String[] dayMonthYear = zuluDate.substring(0, 10).split("-"), hourMinuteSecond = zuluDate.substring(11, 19).split(":");
+                int hour = Integer.valueOf(hourMinuteSecond[0]) + 7;
+
+                //TODO: add configuration options for date and time format, as well as 24 hour time display
+                String formattedDayMonthYear = String.format("%s/%s/%s", dayMonthYear[1], dayMonthYear[2], dayMonthYear[0].replaceFirst("20", "")),
+                formattedHourMinuteSecond = String.format("%s:%s:%s", Math.signum(hour - 12) == -1  || hour - 12 == 0 ? hour : hour - 12, hourMinuteSecond[1], hourMinuteSecond[2]);
+
+                this.setText(String.format("%s\n%s at %s", item.getName(), formattedDayMonthYear, formattedHourMinuteSecond));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
