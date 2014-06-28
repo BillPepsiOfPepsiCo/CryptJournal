0479298f3cd8467190b8073a3c859f00aa799385
DoktuhParadox <doktuhparadox@gmail.com>
2014-06-28 11:42:33 -0500

    Added platform differentiating class

---
+++ src/com/doktuhparadox/cryptjournal/Platform.java
@@ -0,0 +1,29 @@
+package com.doktuhparadox.cryptjournal;
+
+/**
+ * Created and written with IntelliJ IDEA.
+ * Module of: CryptJournal
+ * User: brennanforrest
+ * Date of creation: 6/28/14, at 10:55 AM.
+ */
+public class Platform {
+    private static String getPlatform() {
+        return System.getProperty("os.name");
+    }
+
+    public static boolean isMacOSX() {
+        return getPlatform().contains("osx");
+    }
+
+    public static boolean isWindows() {
+        return getPlatform().contains("win");
+    }
+
+    public static boolean isUnixOrDerivative() {
+        return getPlatform().contains("nix") || getPlatform().contains("nux") || getPlatform().contains("aix");
+    }
+
+    public static boolean isSolaris() {
+        return getPlatform().contains("sunos");
+    }
+}
