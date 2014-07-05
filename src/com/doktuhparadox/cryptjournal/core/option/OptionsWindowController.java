cf630b85f902922c908278d95a82e49af3c5e49a
DoktuhParadox <doktuhparadox@gmail.com>
2014-07-05 10:42:51 -0500

    Added restart logic

--- src/com/doktuhparadox/cryptjournal/core/option/OptionsWindowController.java
+++ src/com/doktuhparadox/cryptjournal/core/option/OptionsWindowController.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 /**
  * Created and written with IntelliJ IDEA.
@@ -30,6 +29,7 @@ public class OptionsWindowController {
     @FXML
     public Button applyButton;
 
+
     @FXML
     protected void initialize() {
         dateFormatTextField.setText(OptionManager.DATE_FORMAT.getValue());
@@ -38,19 +38,18 @@ public class OptionsWindowController {
         twelveHourTimeCheckbox.setSelected(Boolean.valueOf(OptionManager.TWELVE_HOUR_TIME.getValue()));
 
         useDarkThemeCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            if (Dialogs.create().masthead(null).title("Restart?").message("Switching themes requires a restart. Would you like to restart?").showConfirm() == Dialog.Actions.YES) {
+            if (Dialogs.create().masthead(null).title("Restart?").message("Switching themes requires a restart. Would you like to restart?").showConfirm() == Dialog.Actions.YES)
                 RuntimeUtils.restart("CryptJournal.jar");
-            }
+
+            this.onApplyButtonPressed();
         });
     }
 
     @FXML
     public void onApplyButtonPressed() {
-        Stage stage = (Stage) applyButton.getScene().getWindow();
         OptionManager.DATE_FORMAT.setValue(dateFormatTextField.getText());
         OptionManager.TIME_FORMAT.setValue(timeFormatTextField.getText());
         OptionManager.TWELVE_HOUR_TIME.setValue(String.valueOf(twelveHourTimeCheckbox.isSelected()));
         OptionManager.THEME.setValue(useDarkThemeCheckbox.isSelected() ? "dark" : "light");
-        stage.close();
     }
 }
