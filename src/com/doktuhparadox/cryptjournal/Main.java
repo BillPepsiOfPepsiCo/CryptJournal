fdefccb874ba77c692a0107180062469775b112d
DoktuhParadox <doktuhparadox@gmail.com>
2014-07-06 02:10:11 -0500

    App now launches with an icon

--- src/com/doktuhparadox/cryptjournal/Main.java
+++ src/com/doktuhparadox/cryptjournal/Main.java
@@ -1,23 +1,33 @@
 package com.doktuhparadox.cryptjournal;
 
 import com.doktuhparadox.cryptjournal.core.option.OptionManager;
+import com.doktuhparadox.cryptjournal.util.Platform;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.delegateForPlatforms();
         Parent root = FXMLLoader.load(getClass().getResource("core/CryptJournal.fxml"));
         primaryStage.setTitle("CryptJournal");
         primaryStage.setScene(new Scene(root, 1000, 682));
         primaryStage.setResizable(false);
         if (OptionManager.THEME.getValue().equals("dark"))
-            root.getStylesheets().add(getClass().getResource("../../../resources/css/DarkTheme.css").toExternalForm());
+            root.getStylesheets().add(getClass().getResource("/resources/css/DarkTheme.css").toExternalForm());
         primaryStage.show();
+    }
+
+    private void delegateForPlatforms() {
+        if (Platform.isMacOSX()) {
+            com.apple.eawt.Application.getApplication().setDockIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/img/Icon.png")));
+        }
     }
 
 
