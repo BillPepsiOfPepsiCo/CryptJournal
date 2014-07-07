909eb58f8541d98da23246e4dad49373e1cebbb6
DoktuhParadox <doktuhparadox@gmail.com>
2014-07-06 21:07:07 -0500

    Implemented new Easel platform differentiation instead of the Platform class

--- src/com/doktuhparadox/cryptjournal/Main.java
+++ src/com/doktuhparadox/cryptjournal/Main.java
@@ -1,7 +1,8 @@
 package com.doktuhparadox.cryptjournal;
 
 import com.doktuhparadox.cryptjournal.core.option.OptionManager;
-import com.doktuhparadox.cryptjournal.util.Platform;
+import com.doktuhparadox.easel.platform.IPlatformDifferentiator;
+import com.doktuhparadox.easel.platform.PlatformDifferentiator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,11 +11,11 @@ import javafx.stage.Stage;
 
 import java.awt.*;
 
-public class Main extends Application {
+public class Main extends Application implements IPlatformDifferentiator {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        this.delegateForPlatforms();
+        PlatformDifferentiator.setPlatformDifferentiator(this);
         Parent root = FXMLLoader.load(getClass().getResource("core/CryptJournal.fxml"));
         primaryStage.setTitle("CryptJournal");
         primaryStage.setScene(new Scene(root, 1000, 682));
@@ -24,14 +25,27 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    private void delegateForPlatforms() {
-        if (Platform.isMacOSX()) {
-            com.apple.eawt.Application.getApplication().setDockIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/img/Icon.png")));
-        }
-    }
-
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void ifMac() {
+        com.apple.eawt.Application.getApplication().setDockIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/img/Icon.png")));
+    }
+
+    @Override
+    public void ifWindows() {
+
+    }
+
+    @Override
+    public void ifLinux() {
+
+    }
+
+    @Override
+    public void ifSolaris() {
+
     }
 }
